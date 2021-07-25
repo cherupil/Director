dc74a647d02ca3bf87c3326ba933a5c01f3fbf4d
Christopher Cherupil <cherupil@gmail.com>
2021-07-24 23:09:03 -0400

    scroll demo refinements

--- src/js/main.js
+++ src/js/main.js
@@ -26,36 +26,56 @@ imageItems.push({
 	scale: 1,
 	rotateY: 0,
 	rotateX: 0,
-	translateX: 0
+	translateX: 0,
+	translateZ: 0
 })
 imageItems.push({
 	scale: 1,
 	rotateY: 0,
 	rotateX: 0,
-	translateX: 0
+	translateX: 0,
+	translateZ: 0
 })
 imageItems.push({
 	scale: 1,
 	rotateY: 0,
 	rotateX: 0,
-	translateX: 0
+	translateX: 0,
+	translateZ: 0
 })
+
+const gradient = {
+	position: 50,
+	angle: 45,
+	scale: 1,
+	offset: 0
+}
 
 
 
 const copyItemsElements = document.querySelectorAll('.copy-item')
 const imageElements = document.querySelectorAll('.image-item')
 
+const imageWrapper = document.querySelector('.images')
+const root = document.documentElement
+
+
+
 let scrollProgress = 0
 
 const update = () => {
+	imageWrapper.style.transform = `rotateY(${imageItems[0].rotateY}deg) rotateX(${imageItems[0].rotateX}deg)`
 	imageElements.forEach((item, index) => {
-		item.style.transform = `translateX(${imageItems[index].translateX}px) rotateY(${imageItems[index].rotateY}deg) rotateX(${imageItems[index].rotateX}deg) scale(${imageItems[index].scale})`
+		item.style.transform = `translateZ(${imageItems[index].translateZ}px) translateX(${imageItems[index].translateX}px) scale(${imageItems[index].scale})`
 	})
 	copyItemsElements.forEach((item, index) => {
 		item.style.opacity = copyItems[index].opacity
 		item.style.transform = `translateY(${copyItems[index].yOffset}px)`
 	})
+	root.style.setProperty('--gradient-position', `${gradient.position}%`)
+	root.style.setProperty('--gradient-angle', `${gradient.angle}deg`)
+	root.style.setProperty('--gradient-scale', gradient.scale)
+	root.style.setProperty('--gradient-offset', `${gradient.offset}px`)
 	requestAnimationFrame(update)
 	composition.setProgress(scrollProgress)
 }
@@ -76,12 +96,14 @@ copyItems.forEach((item, index) => {
 		ease: 'linear'
 	}, 0.75 + index)
 })
-composition.from(imageItems, { scale: 0 }, { duration: 1, ease: 'linear' }, 0)
-composition.to(imageItems[0], { translateX: -250 }, { duration: 1, ease: 'linear' }, 1)
-composition.to(imageItems[2], { translateX: 250 }, { duration: 1, ease: 'linear' }, 1)
-composition.to(imageItems[0], { translateX: -125 }, { duration: 1, ease: 'linear' }, 2)
-composition.to(imageItems[2], { translateX: 125 }, { duration: 1, ease: 'linear' }, 2)
-composition.to(imageItems, { rotateY: 30, rotateX: 30 }, { duration: 1, ease: 'linear' }, 2)
+composition.from(imageItems, { scale: 0 }, { duration: 1, ease: 'easeOutExpo' }, 0)
+composition.to(imageItems[0], { translateX: -320, translateZ: -250 }, { duration: 1, ease: 'easeOutBack' }, 1)
+composition.to(imageItems[1], { translateX: 320, translateZ: -250 }, { duration: 1, ease: 'easeOutBack' }, 1)
+composition.to(imageItems[0], { translateX: -280 }, { duration: 1, ease: 'easeOutSine' }, 2)
+composition.to(imageItems[1], { translateX: 280 }, { duration: 1, ease: 'easeOutSine' }, 2)
+composition.to(imageItems, { rotateY: 30, rotateX: 30 }, { duration: 1, ease: 'easeOutSine' }, 2)
+composition.to(gradient, { position: 25, angle: 30, offset: 4 }, { duration: 1, ease: 'easeOutSine' }, 2)
+composition.to(imageItems[2], { translateZ: 80}, { duration: 1, ease: 'easeOutSine' }, 2)
 
 
 const stickyElement = document.querySelector('.sticky-element')
